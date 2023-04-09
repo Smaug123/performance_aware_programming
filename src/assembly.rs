9b9908947dbc6c3faa4d005237ed55fc1802c9b4
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-04-09 11:28:21 +0100

    WIP

--- src/assembly.rs
+++ src/assembly.rs
@@ -1,20 +1,34 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{char, digit1, line_ending, not_line_ending, one_of},
+    character::complete::{char, digit1, line_ending, multispace0, not_line_ending, one_of},
     combinator::map_res,
     multi::many0,
-    sequence::{preceded, separated_pair, terminated, tuple},
+    sequence::{delimited, preceded, separated_pair, terminated, tuple},
     IResult,
 };
 
 use crate::{
     register::{ByteRegisterSubset, GeneralRegister, Register, RegisterSubset, SpecialRegister},
-    Instruction, MoveInstruction, Program,
+    Base, EffectiveAddress, Instruction, MemRegMove, Program, RegMemMove, RegRegMove, SourceDest,
+    WithOffset,
 };
 
 fn comment(input: &str) -> IResult<&str, &str> {
     preceded(char(';'), terminated(not_line_ending, line_ending))(input)
+}
+
+fn ws<'a, F, O, E: nom::error::ParseError<&'a str>>(
+    inner: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+where
+    F: FnMut(&'a str) -> IResult<&'a str, O, E>,
+{
+    delimited(multispace0, inner, multispace0)
+}
+
+fn argument_sep(input: &str) -> IResult<&str, ()> {
+    map_res(ws(char(',')), |_| Ok::<_, ()>(()))(input)
 }
 
 fn bits(input: &str) -> IResult<&str, u8> {
@@ -67,18 +81,182 @@ fn register(input: &str) -> IResult<&str, Register> {
     ))(input)
 }
 
-fn move_instruction(input: &str) -> IResult<&str, MoveInstruction> {
+fn reg_reg_move_instruction(input: &str) -> IResult<&str, RegRegMove> {
     map_res(
         preceded(
             tag("mov "),
-            tuple((register, tag(", "), register, line_ending)),
+            tuple((register, argument_sep, register, line_ending)),
         ),
-        |(dest, _, source, _)| Ok::<_, ()>(MoveInstruction { dest, source }),
+        |(dest, _, source, _)| Ok::<_, ()>(RegRegMove { dest, source }),
+    )(input)
+}
+
+fn direct_offset(input: &str) -> IResult<&str, u16> {
+    preceded(
+        char('['),
+        terminated(map_res(digit1, str::parse), char(']')),
+    )(input)
+}
+
+fn base(input: &str) -> IResult<&str, Base> {
+    alt((
+        map_res(tag("bx"), |_| Ok::<_, ()>(Base::Bx)),
+        map_res(tag("bp"), |_| Ok::<_, ()>(Base::Bp)),
+    ))(input)
+}
+
+fn source_dest(input: &str) -> IResult<&str, SourceDest> {
+    alt((
+        map_res(char('s'), |_| Ok::<_, ()>(SourceDest::Source)),
+        map_res(char('d'), |_| Ok::<_, ()>(SourceDest::Dest)),
+    ))(input)
+}
+
+fn absolute_u8(input: &str) -> IResult<&str, u8> {
+    map_res(digit1, |digits| str::parse::<u8>(digits))(input)
+}
+
+fn absolute_u16(input: &str) -> IResult<&str, u16> {
+    map_res(digit1, |digits| str::parse::<u16>(digits))(input)
+}
+
+fn effective_address(input: &str) -> IResult<&str, EffectiveAddress> {
+    alt((
+        // Sum, no offset
+        map_res(
+            tuple((base, ws(char('+')), source_dest)),
+            |(base, _, source_dest)| {
+                Ok::<_, ()>(EffectiveAddress::Sum(WithOffset::Basic((
+                    base,
+                    source_dest,
+                ))))
+            },
+        ),
+        // Sum, with positive offset
+        map_res(
+            tuple((base, ws(char('+')), source_dest, ws(char('+')), absolute_u8)),
+            |(base, _, source_dest, _, offset)| {
+                Ok::<_, ()>(EffectiveAddress::Sum(WithOffset::WithU8(
+                    (base, source_dest),
+                    offset,
+                )))
+            },
+        ),
+        map_res(
+            tuple((
+                base,
+                ws(char('+')),
+                source_dest,
+                ws(char('+')),
+                absolute_u16,
+            )),
+            |(base, _, source_dest, _, offset)| {
+                Ok::<_, ()>(EffectiveAddress::Sum(WithOffset::WithU16(
+                    (base, source_dest),
+                    offset,
+                )))
+            },
+        ),
+        // Lookup
+        map_res(source_dest, |source_dest| {
+            Ok::<_, ()>(EffectiveAddress::SpecifiedIn(WithOffset::Basic(
+                source_dest,
+            )))
+        }),
+        map_res(
+            tuple((source_dest, ws(char('+')), absolute_u8)),
+            |(source_dest, _, offset)| {
+                Ok::<_, ()>(EffectiveAddress::SpecifiedIn(WithOffset::WithU8(
+                    source_dest,
+                    offset,
+                )))
+            },
+        ),
+        map_res(
+            tuple((source_dest, ws(char('+')), absolute_u16)),
+            |(source_dest, _, offset)| {
+                Ok::<_, ()>(EffectiveAddress::SpecifiedIn(WithOffset::WithU16(
+                    source_dest,
+                    offset,
+                )))
+            },
+        ),
+        // Offset from BX
+        map_res(tag("bx"), |_| {
+            Ok::<_, ()>(EffectiveAddress::Bx(WithOffset::Basic(())))
+        }),
+        map_res(
+            tuple((tag("bx"), ws(char('+')), absolute_u8)),
+            |(_, _, offset)| Ok::<_, ()>(EffectiveAddress::Bx(WithOffset::WithU8((), offset))),
+        ),
+        map_res(
+            tuple((tag("bx"), ws(char('+')), absolute_u16)),
+            |(_, _, offset)| Ok::<_, ()>(EffectiveAddress::Bx(WithOffset::WithU16((), offset))),
+        ),
+        // Direct memory address
+        map_res(direct_offset, |offset| {
+            Ok::<_, ()>(EffectiveAddress::Direct(offset))
+        }),
+        // Offset from base pointer
+        map_res(
+            tuple((tag("bp"), ws(char('+')), absolute_u8)),
+            |(_, _, offset)| Ok::<_, ()>(EffectiveAddress::BasePointer(offset)),
+        ),
+        map_res(
+            tuple((tag("bp"), ws(char('+')), absolute_u16)),
+            |(_, _, offset)| Ok::<_, ()>(EffectiveAddress::BasePointerWide(offset)),
+        ),
+    ))(input)
+}
+
+fn reg_mem_move_instruction(input: &str) -> IResult<&str, RegMemMove> {
+    map_res(
+        preceded(
+            tag("mov "),
+            tuple((
+                terminated(effective_address, argument_sep),
+                terminated(register, line_ending),
+            )),
+        ),
+        |(address, register)| {
+            Ok::<_, ()>(RegMemMove {
+                dest: address,
+                source: register,
+            })
+        },
+    )(input)
+}
+
+fn mem_reg_move_instruction(input: &str) -> IResult<&str, MemRegMove> {
+    map_res(
+        preceded(
+            tag("mov "),
+            tuple((
+                terminated(register, argument_sep),
+                terminated(effective_address, line_ending),
+            )),
+        ),
+        |(register, address)| {
+            Ok::<_, ()>(MemRegMove {
+                dest: register,
+                source: address,
+            })
+        },
     )(input)
 }
 
 fn instruction(input: &str) -> IResult<&str, Instruction> {
-    map_res(move_instruction, |v| Ok::<_, ()>(Instruction::Move(v)))(input)
+    alt((
+        map_res(reg_reg_move_instruction, |v| {
+            Ok::<_, ()>(Instruction::RegRegMove(v))
+        }),
+        map_res(reg_mem_move_instruction, |v| {
+            Ok::<_, ()>(Instruction::RegMemMove(v))
+        }),
+        map_res(mem_reg_move_instruction, |v| {
+            Ok::<_, ()>(Instruction::MemRegMove(v))
+        }),
+    ))(input)
 }
 
 fn trivia(input: &str) -> IResult<&str, &str> {
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,141 @@
 mod assembly;
 mod register;
 
-use std::{fmt::Display, fs, path::Path};
+use std::{
+    fmt::{Display, Write},
+    fs,
+    path::Path,
+};
 
 use clap::Parser;
 use register::{ByteRegisterSubset, Register, RegisterSubset};
 
 #[derive(Eq, PartialEq)]
-pub struct MoveInstruction {
+pub struct RegRegMove {
     source: Register,
     dest: Register,
 }
 
 #[derive(Eq, PartialEq)]
+pub enum SourceDest {
+    Source,
+    Dest,
+}
+
+impl Display for SourceDest {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SourceDest::Source => f.write_char('s'),
+            SourceDest::Dest => f.write_char('d'),
+        }
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub enum Base {
+    Bx,
+    Bp,
+}
+
+impl Display for Base {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Base::Bx => f.write_str("bx"),
+            Base::Bp => f.write_str("bp"),
+        }
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub enum WithOffset<T> {
+    Basic(T),
+    WithU8(T, u8),
+    WithU16(T, u16),
+}
+
+impl<T> Display for WithOffset<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            WithOffset::Basic(t) => f.write_fmt(format_args!("{}", t)),
+            WithOffset::WithU8(t, offset) => f.write_fmt(format_args!("[{} + {}]", t, offset)),
+            WithOffset::WithU16(t, offset) => f.write_fmt(format_args!("[{} + {}]", t, offset)),
+        }
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub enum EffectiveAddress {
+    Sum(WithOffset<(Base, SourceDest)>),
+    SpecifiedIn(WithOffset<SourceDest>),
+    Bx(WithOffset<()>),
+    Direct(u16),
+    BasePointer(u8),
+    BasePointerWide(u16),
+}
+
+impl Display for EffectiveAddress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EffectiveAddress::Sum(w) => match w {
+                WithOffset::Basic((base, source_dest)) => {
+                    f.write_fmt(format_args!("{}{}", base, source_dest))
+                }
+                WithOffset::WithU8((base, source_dest), offset) => {
+                    f.write_fmt(format_args!("[{}{} + {}]", base, source_dest, offset))
+                }
+                WithOffset::WithU16((base, source_dest), offset) => {
+                    f.write_fmt(format_args!("[{}{} + {}]", base, source_dest, offset))
+                }
+            },
+            EffectiveAddress::SpecifiedIn(register) => f.write_fmt(format_args!("{}", register)),
+            EffectiveAddress::Bx(offset) => match offset {
+                WithOffset::Basic(()) => f.write_str("bx"),
+                WithOffset::WithU8((), offset) => f.write_fmt(format_args!("[bx + {}]", offset)),
+                WithOffset::WithU16((), offset) => f.write_fmt(format_args!("[bx + {}]", offset)),
+            },
+            EffectiveAddress::Direct(location) => f.write_fmt(format_args!("[{}]", location)),
+            EffectiveAddress::BasePointer(offset) => f.write_fmt(format_args!("[bp + {}]", offset)),
+            EffectiveAddress::BasePointerWide(offset) => {
+                f.write_fmt(format_args!("[bp + {}]", offset))
+            }
+        }
+    }
+}
+
+#[derive(Eq, PartialEq)]
+pub struct RegMemMove {
+    source: Register,
+    dest: EffectiveAddress,
+}
+
+#[derive(Eq, PartialEq)]
+pub struct MemRegMove {
+    source: EffectiveAddress,
+    dest: Register,
+}
+
+#[derive(Eq, PartialEq)]
 pub enum Instruction {
-    Move(MoveInstruction),
+    RegRegMove(RegRegMove),
+    RegMemMove(RegMemMove),
+    MemRegMove(MemRegMove),
 }
 
 impl Display for Instruction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Instruction::Move(mov) => f.write_fmt(format_args!("mov {}, {}", mov.dest, mov.source)),
+            Instruction::RegRegMove(mov) => {
+                f.write_fmt(format_args!("mov {}, {}", mov.dest, mov.source))
+            }
+            Instruction::RegMemMove(mov) => {
+                f.write_fmt(format_args!("mov {}, {}", mov.dest, mov.source))
+            }
+            Instruction::MemRegMove(mov) => {
+                f.write_fmt(format_args!("mov {}, {}", mov.dest, mov.source))
+            }
         }
     }
 }
@@ -28,7 +143,7 @@ impl Display for Instruction {
 impl Instruction {
     fn to_bytes(&self) -> Vec<u8> {
         match self {
-            Instruction::Move(mov) => {
+            Instruction::RegRegMove(mov) => {
                 let mut result = Vec::with_capacity(2);
                 let instruction1 = 0b10001000u8;
                 let mut is_wide = 1u8;
@@ -121,24 +236,104 @@ impl Instruction {
                     let mode = (mod_reg_rm & 0b11000000) / 64;
                     let reg = (mod_reg_rm & 0b00111000) / 8;
                     let rm = mod_reg_rm & 0b00000111;
+                    let reg = Register::of_id(reg, is_wide);
 
                     if mode == 3 {
                         let rm = Register::of_id(rm, is_wide);
-                        let reg = Register::of_id(reg, is_wide);
+
                         let instruction = if d == 0 {
-                            MoveInstruction {
+                            RegRegMove {
                                 source: reg,
                                 dest: rm,
                             }
                         } else {
-                            MoveInstruction {
+                            RegRegMove {
                                 source: rm,
                                 dest: reg,
                             }
                         };
-                        Some(Instruction::Move(instruction))
+                        Some(Instruction::RegRegMove(instruction))
                     } else {
-                        panic!("Unimplemented mode {} for instruction {}, we only know register-register mov", mode, b)
+                        let source_dest = if rm % 2 == 0 {
+                            SourceDest::Source
+                        } else {
+                            SourceDest::Dest
+                        };
+                        let base = if (rm / 2) % 2 == 0 {
+                            Base::Bx
+                        } else {
+                            Base::Bp
+                        };
+                        let displacement_high = if rm == 6 || mode > 0 {
+                            bytes.next().expect("required an 8-bit displacement")
+                        } else {
+                            0
+                        };
+                        let displacement_low = if rm == 6 || mode == 2 {
+                            let low = bytes.next().expect("required a 16-bit displacement");
+                            (displacement_high as u16) * 256 + (low as u16)
+                        } else {
+                            0
+                        };
+
+                        let mem_location = if rm < 4 {
+                            match mode {
+                                0 => EffectiveAddress::Sum(WithOffset::Basic((base, source_dest))),
+                                1 => EffectiveAddress::Sum(WithOffset::WithU8(
+                                    (base, source_dest),
+                                    displacement_high,
+                                )),
+                                2 => EffectiveAddress::Sum(WithOffset::WithU16(
+                                    (base, source_dest),
+                                    displacement_low,
+                                )),
+                                _ => panic!("Maths is wrong, got bad mode: {}", mode),
+                            }
+                        } else if rm < 6 {
+                            match mode {
+                                0 => EffectiveAddress::SpecifiedIn(WithOffset::Basic(source_dest)),
+                                1 => EffectiveAddress::SpecifiedIn(WithOffset::WithU8(
+                                    source_dest,
+                                    displacement_high,
+                                )),
+                                2 => EffectiveAddress::SpecifiedIn(WithOffset::WithU16(
+                                    source_dest,
+                                    displacement_low,
+                                )),
+                                _ => panic!("Maths is wrong, got bad mode: {}", mode),
+                            }
+                        } else if rm == 6 {
+                            match mode {
+                                0 => EffectiveAddress::Direct(displacement_low),
+                                1 => EffectiveAddress::BasePointer(displacement_high),
+                                2 => EffectiveAddress::BasePointerWide(displacement_low),
+                                _ => panic!("Maths is wrong, got bad mode: {}", mode),
+                            }
+                        } else {
+                            assert!(rm == 7);
+                            match mode {
+                                0 => EffectiveAddress::Bx(WithOffset::Basic(())),
+                                1 => {
+                                    EffectiveAddress::Bx(WithOffset::WithU8((), displacement_high))
+                                }
+                                2 => {
+                                    EffectiveAddress::Bx(WithOffset::WithU16((), displacement_low))
+                                }
+                                _ => panic!("Maths is wrong, got bad mode: {}", mode),
+                            }
+                        };
+
+                        if d == 0 {
+                            Some(Instruction::RegMemMove(RegMemMove {
+                                source: reg,
+                                dest: mem_location,
+                            }))
+                        } else {
+                            Some(Instruction::MemRegMove(MemRegMove {
+                                dest: reg,
+                                source: mem_location,
+                            }))
+                        }
                     }
                 } else {
                     panic!("mov required a second byte")
@@ -284,20 +479,17 @@ mod test_program {
 
     use super::assembly::program;
 
-    #[test]
-    fn test_register_register_mov_parser() {
-        let input_asm = include_str!(
-            "../computer_enhance/perfaware/part1/listing_0037_single_register_mov.asm"
-        );
-        let input_bytecode =
-            include_bytes!("../computer_enhance/perfaware/part1/listing_0037_single_register_mov");
+    fn test_parser<T>(input_asm: &str, input_bytecode: T)
+    where
+        T: AsRef<[u8]>,
+    {
         let (remaining, parsed) = program(input_asm).unwrap();
         assert_eq!(remaining, "");
         assert_eq!(parsed.bits, 16);
         for (i, (actual, expected)) in parsed
             .to_bytes()
             .iter()
-            .zip(input_bytecode.iter())
+            .zip(input_bytecode.as_ref().iter())
             .enumerate()
         {
             if actual != expected {
@@ -309,17 +501,14 @@ mod test_program {
         }
     }
 
-    #[test]
-    fn test_register_register_mov_disassembler() {
-        let bytecode =
-            include_bytes!("../computer_enhance/perfaware/part1/listing_0037_single_register_mov");
-        let asm = include_str!(
-            "../computer_enhance/perfaware/part1/listing_0037_single_register_mov.asm"
-        );
-        let (remaining, pre_compiled) = program(&asm).unwrap();
+    fn test_disassembler<T>(input_asm: &str, input_bytecode: T)
+    where
+        T: AsRef<[u8]>,
+    {
+        let (remaining, pre_compiled) = program(&input_asm).unwrap();
         assert_eq!(remaining, "");
 
-        let disassembled = Program::of_bytes(bytecode.iter().cloned());
+        let disassembled = Program::of_bytes(input_bytecode.as_ref().iter().cloned());
 
         if disassembled != pre_compiled {
             panic!(
@@ -327,5 +516,25 @@ mod test_program {
                 disassembled, pre_compiled
             );
         }
+    }
+
+    #[test]
+    fn test_register_register_mov_parser() {
+        let input_asm = include_str!(
+            "../computer_enhance/perfaware/part1/listing_0037_single_register_mov.asm"
+        );
+        let input_bytecode =
+            include_bytes!("../computer_enhance/perfaware/part1/listing_0037_single_register_mov");
+        test_parser(input_asm, input_bytecode)
+    }
+
+    #[test]
+    fn test_register_register_mov_disassembler() {
+        let bytecode =
+            include_bytes!("../computer_enhance/perfaware/part1/listing_0037_single_register_mov");
+        let asm = include_str!(
+            "../computer_enhance/perfaware/part1/listing_0037_single_register_mov.asm"
+        );
+        test_disassembler(asm, bytecode)
     }
 }
