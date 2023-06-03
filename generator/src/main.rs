09ba1a1f96340215f175f2600593c41fc1f382f6
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-06-03 14:23:38 +0100

    Basic binary framework

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,3 +1,61 @@
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
+use clap::{Parser, ValueEnum, builder::PossibleValue};
+
+#[derive(Clone, Debug)]
+enum SelectionAlgorithm {
+    Cluster,
+    Uniform
+}
+
+impl Display for SelectionAlgorithm {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Cluster => {
+                f.write_str("cluster")
+            }
+            Self::Uniform => {
+                f.write_str("uniform")
+            }
+        }
+    }
+}
+
+impl FromStr for SelectionAlgorithm {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_str() {
+            "cluster" => Ok(SelectionAlgorithm::Cluster),
+            "uniform" => Ok(SelectionAlgorithm::Uniform),
+            _ => Err(())
+        }
+    }
+}
+
+impl ValueEnum for SelectionAlgorithm {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[SelectionAlgorithm::Cluster, SelectionAlgorithm::Uniform]
+    }
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        match self {
+            SelectionAlgorithm::Cluster => Some(PossibleValue::new("cluster")),
+            SelectionAlgorithm::Uniform => Some(PossibleValue::new("uniform"))
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(value_name = "RANDOM_SEED", required = true)]
+    seed: u32,
+    #[arg(value_name = "COUNT", required = true)]
+    count: usize,
+    #[arg(default_value_t = SelectionAlgorithm::Cluster, long)]
+    algorithm: SelectionAlgorithm,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, world! {:?}", args);
 }
